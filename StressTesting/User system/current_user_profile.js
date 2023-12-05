769720ef7363ca3e7fe7e6933b168f5eb5064bfb
mahmoudossama10 <59567436+mahmoudossama10@users.noreply.github.com>
2023-12-05 21:08:14 +0200

    Add files via upload

---
+++ StressTesting/User system/current_user_profile.js
@@ -0,0 +1,64 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+import { check } from 'k6';
+import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.2/index.js";
+// export const options={
+//     vus: 10,
+//     duration:'10s',
+// }
+
+export const options = {
+  scenarios: {
+    shared_iter_scenario: {
+      executor: "shared-iterations",
+      vus: 10,
+      iterations: 100,
+      startTime: "0s",
+    },
+    per_vu_scenario: {
+      executor: "per-vu-iterations",
+      vus: 10,
+      iterations: 10,
+      startTime: "10s",
+    },
+  },
+};
+
+ const userEmail = "mahmoud.khattab13@gmail.com";
+ const userPassword = "deaddead";
+
+export default function getToken() {
+  const url = 'http://backend.gigachat.cloudns.org/api/user/profile';
+  
+  const payload = JSON.stringify({
+
+  });
+
+  const params = {
+    headers: {
+      "Content-Type": "application/json",
+      "authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1NjNhMzcwZGJkYzA2YzkyZjg2ZTRhYiIsImlhdCI6MTcwMTc3NDUwMSwiZXhwIjoxNzA5NTUwNTAxfQ.u8kUPL5dWE4mgWhN85rW0h8m6aEXl8S-VtN_dgKepw0",
+    },
+    username: 'mahmoud',
+  };
+
+  const res = http.get(url,params);
+
+  check(res, {
+    "Status is 200": (r) => r.status === 200,
+    //"Response includes token": (r) => JSON.parse(r.body).token !== undefined,
+  });
+  //const token = JSON.parse(res.body).token;
+  console.log(res.status);
+  console.log(res.body);
+  sleep(1);
+
+ // return token;
+}
+export function handleSummary(data) {
+  return {
+    stdout: textSummary(data, { indent: " ", enableColors: true }), // Show the text summary to stdout...
+    "CurrentUserProfileReport.json": JSON.stringify(data), //the default data object
+  };
+}
+  //sleep(1);
