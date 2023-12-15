a696aa1379eadda876a4db72c67d66425bd48e38
Ibraheim <77370509+IbraheimTarek@users.noreply.github.com>
2023-12-15 18:08:04 +0200

    get chat in DM

---
+++ StressTesting/Direct Messages/getChatStress.js
@@ -0,0 +1,40 @@
+import encoding from "k6/encoding";
+import http from "k6/http";
+import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.2/index.js";
+import { check, sleep, group } from "k6";
+import getToken from "../Tweet/loginStress.js";
+import TestOptions from "../Tweet/options.js";
+
+const optionsInstance = new TestOptions();
+export const options = optionsInstance.getOptions();
+
+export default function () {
+  group("Get chat", function () {
+    const authToken = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1NzQ2ZjA0NGUyOGRlYTYyMDY5M2I4MSIsImlhdCI6MTcwMjEyOTQ3MiwiZXhwIjoxNzA5OTA1NDcyfQ.hn1CqfcPfGvFZuDn7PBhNfIpjv_ObO2SfZre3v0Y6FQ`;
+    const url = `http://backend.gigachat.cloudns.org/api/user/chat/65746e864e28dea620693b2d`;
+
+    const params = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${authToken}`,
+      },
+    };
+
+    const response = http.get(url, params);
+
+    check(response, {
+      "Status is 200 or 400": (r) => r.status === 200 || r.status === 400,
+    });
+
+    console.log(response.body);
+    sleep(1);
+  });
+}
+
+export function handleSummary(data) {
+  return {
+    stdout: textSummary(data, { indent: " ", enableColors: true }), // Show the text summary to stdout...
+    "getChatReport160VUs.json": JSON.stringify(data), //the default data object
+  };
+}
+//k6 run --vus 1 --duration 1m TweetStress.js
